120b19eec2d1e3850aea639ecdddc0e40e65f01b
basakpie <basakpie@gmail.com>
2017-02-17 12:14:43 +0900

    02장. 프로젝트 시작하기

---
+++ vaadin-seminar/src/main/java/com/vseminar/VSeminarUI.java
@@ -0,0 +1,55 @@
+package com.vseminar;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("vseminar")
+@Widgetset("com.vseminar.VSeminarWidgetset")
+@SuppressWarnings("serial")
+public class VSeminarUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        final TextField name = new TextField();
+        name.setCaption("Type your name here:");
+
+        Button button = new Button("Click Me");
+        button.addClickListener(new ClickListener() {
+            @Override   
+            public void buttonClick(ClickEvent event) {     
+                layout.addComponent(new Label("Thanks " + name.getValue() + ", it works!"));
+            }
+        });
+        layout.addComponents(name, button);
+        layout.setMargin(true);
+        layout.setSpacing(true);
+        
+        setContent(layout);
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "VSeminarUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = VSeminarUI.class, productionMode = false)
+    public static class VSeminarUIServlet extends VaadinServlet {
+    }
+}
